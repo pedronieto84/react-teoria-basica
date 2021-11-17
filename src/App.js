682c593610e6157091f5d31bba18b00794870c1a
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-11-17 16:48:00 +0100

    Pasar props de Padre a hijo (Componente Funcional)

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import ClassComponent from "./components/classComponent";
 import FunctionalComponent from "./components/functionalComponent";
 
 function App() {
+  const propFuncString = "soc un prop string";
+  const propFuncObject = {
+    que: "soc un prop Func OBJECT",
+  };
   return (
     <>
-      <FunctionalComponent />
+      <FunctionalComponent
+        propFuncString={propFuncString}
+        propFuncObject={propFuncObject}
+        propFuncNumber={3}
+      />
       <ClassComponent />
     </>
   );
--- src/components/functionalComponent.js
+++ src/components/functionalComponent.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-const FunctionalComponent = () => {
+const FunctionalComponent = (props) => {
   return (
     <>
-      <h1>Soc un functional Component </h1>
+      <h1> Soc un functional Component </h1>
+      <p> Props String: { props.propFuncString } </p>
+      <p> Props Object: { props.propFuncObject.que } </p>
+      <p> Props Number: { props.propFuncNumber } </p>
     </>
-  )
+  );
 };
 
 export default FunctionalComponent;
