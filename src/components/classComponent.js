e99e8007dfd34767265e76f8d40c512a48f9c7e5
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-11-17 16:40:30 +0100

    Implementació Bootstrap.

--- src/components/classComponent.js
+++ src/components/classComponent.js
@@ -4,7 +4,7 @@ class ClassComponent extends React.Component {
   render() {
     return (
       <>
-        <h1>Soc un class component</h1>;{" "}
+        <h1 class="card">Soc un class component</h1>;{" "}
       </>
     );
   }
