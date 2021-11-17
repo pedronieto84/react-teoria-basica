9ef203001b8f1e2c615ce5d7b6f7c1e1ad443d18
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-11-17 19:26:23 +0100

    Mounting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import "./App.css";
 
 import ClassComponent from "./components/classComponent";
+import React, { Component } from "react";
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { data: 0 };
+  }
 
-  
-
-
-
-  return (
-    <>
-      <ClassComponent/>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <button
+          onClick={() => {
+            this.setState({ data: this.state.data + 1 });
+          }}
+        >
+          AÑADIR
+        </button>
+        <ClassComponent data={this.state.data} />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/classComponent.js
+++ src/components/classComponent.js
@@ -1,23 +1,31 @@
 import React, { Component } from "react";
 
 class ClassComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { nom: "Joan" };
+  constructor() {
+    super();
+    this.state = { valueTwice: 0 };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("getDerivedStateFromProps");
+    return { valueTwice: props.data * 2 };
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ nom: "Antoni" });
-    }, 3000);
+    console.log("component did mount");
   }
 
   render() {
+    console.log("render");
     return (
       <>
-        <h1 class="card">State: {this.state.nom} </h1>;
+        <h1 class="card">State: {this.state.valueTwice} </h1>;
       </>
     );
+  }
+
+  componentDidMount() {
+    console.log("component did mount");
   }
 }
 
