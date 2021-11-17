9e96a6bfe479ec04f0ad2a8a011bc1bc2d5ca257
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-11-17 16:35:40 +0100

    Creacion e implementacion de Functional Component y class Component.

---
+++ src/components/classComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ClassComponent extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Soc un class component</h1>;{" "}
+      </>
+    );
+  }
+}
+
+export default ClassComponent;
---
+++ src/components/functionalComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FunctionalComponent = () => {
+  return (
+    <>
+      <h1>Soc un functional Component </h1>
+    </>
+  )
+};
+
+export default FunctionalComponent;
