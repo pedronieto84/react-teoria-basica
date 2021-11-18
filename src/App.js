669ce9f5a4289059fcda3bad6ed455fe40c2e658
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-11-18 07:21:10 +0100

    Rutes dinàmiques

--- src/App.js
+++ src/App.js
@@ -5,28 +5,37 @@ import FunctionalComponent from "./components/functionalComponent";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-      <nav>
-        <Link to="/">Class </Link>
-        <Link to="/functional">Functional</Link>
-      </nav>
-      <div>
-        <Switch>
-          <Route exact path="/functional">
-            <FunctionalComponent />
-          </Route>
-          <Route exact path="/class">
-            <ClassComponent />
-          </Route>
-          <Route exact path="/">
-            <ClassComponent />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { id: 1 };
+  }
+
+  render() {
+    const { id } = this.state;
+    return (
+      <Router>
+        <nav>
+          <Link to="/">Class </Link>
+          <Link to="/functional">Functional</Link>
+          <Link to={`/dinamic/${id}`}>Dinamic</Link>
+        </nav>
+        <div>
+          <Switch>
+            <Route exact path="/functional">
+              <FunctionalComponent />
+            </Route>
+            <Route exact path="/class">
+              <ClassComponent />
+            </Route>
+            <Route exact path="/dinamic/:id">
+              <ClassComponent />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
