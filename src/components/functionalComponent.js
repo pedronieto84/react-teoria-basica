aa54d12de795a0c6fb768cc08a3c505753661dc8
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-11-18 07:33:07 +0100

    Hooks: UseState.

--- src/components/functionalComponent.js
+++ src/components/functionalComponent.js
@@ -1,14 +1,35 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
 
 const FunctionalComponent = () => {
- 
-  console.log("params", useParams());
 
-  const { id } = useParams();
+  const [count, setCount] = useState(0);
+  const [paraula, setParaula] = useState("hola");
+
   return (
     <>
-      <h1> Soc un dinamic Component i el meu param es: {id} </h1>
+      <h1> Soc un Functional Component </h1>
+
+      <div>
+        <h1>Count {count}</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Increase Count
+        </button>
+      </div>
+      <br />
+      <div>
+        <h1>Count {paraula}</h1>
+        <button
+          onClick={() => {
+            setParaula(paraula + "a");
+          }}
+        >
+          Afegir A
+        </button>
+      </div>
     </>
   );
 };
