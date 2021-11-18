b65a077a640ee8fff04245b7849309ad8698b8c2
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-11-18 07:34:58 +0100

    Hooks: UseEffect

--- src/components/functionalComponent.js
+++ src/components/functionalComponent.js
@@ -1,35 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 
 const FunctionalComponent = () => {
-
-  const [count, setCount] = useState(0);
-  const [paraula, setParaula] = useState("hola");
+  useEffect(() => {
+    console.log("use effect");
+  });
 
   return (
     <>
       <h1> Soc un Functional Component </h1>
 
-      <div>
-        <h1>Count {count}</h1>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          Increase Count
-        </button>
-      </div>
-      <br />
-      <div>
-        <h1>Count {paraula}</h1>
-        <button
-          onClick={() => {
-            setParaula(paraula + "a");
-          }}
-        >
-          Afegir A
-        </button>
-      </div>
+      <div>USE EFFECT</div>
     </>
   );
 };
